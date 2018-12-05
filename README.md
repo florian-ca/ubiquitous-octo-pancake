470e8c11b7680b8aef8125268a527bd3556e95e7
Florian BIANCIOTTO <florian.bianciotto@orange.fr>
2018-12-05 13:00:38 +0100

    version final parser + readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ubiquitous-octo-pancake
 TP CERI - Génie Logiciel Scrum - Parseur d’articles scientifiques en format texte 
+
+Parser utilisé : pdftotext
+langage utilisé : c++
+fichier c++:parserpdf.cpp
+
+Commande de compilation :
+g++ -std=c++11 parserpdf.cpp -o parserpdf
+
+Lancer le programme :
+./parserpdf pdftotext fileName.pdf fileOutput.txt
+avec :
+filename.pdf le nom du fichier à parser (au format pdf)
+fileOutput.txt le fichier de sortie
+
+Le document parsé et résumé se trouve dans fileParsed.txt
+
+
+Le parser fonctionne pour les fichiers :
+-Lin_2004_Rouge.pdf
+-Doyle_2005_Automatic Categorization of Author Gender.pdf
+-Torres-Moreno_2012_Artex is another text summarizer.pdf
+-Levner_2007_Fuzzifying clustering algorithms The case study of MajorClust.pdf
+-Alexandrov_2015_A Modified Tripartite Model for Document Representation in Internet Sociology.pdf
+-Mikolov_2013_Distributed representations of words and phrases and their compositionality.pdf
+
+Les autres fichiers sont trop compliqués à parser.
+Soit le titre n'est pas sur la premiere ligne soit l'auteur n'est pas directement après le titre.
--- parserpdf.cpp
+++ parserpdf.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <chrono>
 #include <fstream>
+#include <locale>
 
 using namespace std;
 
@@ -18,7 +19,7 @@ int main(int argc, char ** argv)
 	{
 		wait(&status);
 		ifstream fichierout(argv[argc-1]);
-		ofstream fichierin("paperstest.txt");
+		ofstream fichierin("fileParsed.txt");
 	    if(fichierout && fichierin)
 	    {
 			int line=0;
@@ -27,21 +28,49 @@ int main(int argc, char ** argv)
 
 	    	while(getline(fichierout,s))
 	    	{
-	    		if(line==0)
+
+	    		std::locale loc("C");
+	    		if(line==0) // 1er ligne = ligne du titre
 	    		{
-	    			fichierin<<"Titre: "<<s<<endl;
+
+	    			fichierin<<"Titre: "<<s;
 	    		}
-	    		else if(line==1)
+	    		// si les deux premiers mot commencent par une majuscule i.e ligne de l'auteur
+	    		if(line==1 && !std::islower(s.at(0),loc) && !std::islower(s.at(s.find(" ",0)+1),loc) ) 
 	    		{
-	    			fichierin<<"Auteur: "<<s<<endl;
+	    			cout << "uppercase"<<endl;
+	    			int a=s.find(" ",0);
+	    			cout << a<<endl;
+
+	    			fichierin<<endl<<"Auteur: "<<s<<endl;
 	    		}
-	    		else
+
+	    		// si l'un des deux premier mot de la ligne commence pas par une majuscule i.e pas la ligne de l'auteur mais 2nde ligne du titre
+	    		
+	    		else if (line==1 && (std::islower(s.at(0),loc) || std::islower(s.at(s.find(" ",0)+1),loc)) ) 
 	    		{
+
+	    			cout << "lowercase"<<endl;
+	    			fichierin<<" "<<s<<endl;
+	    			string S="";
+	    			while(S=="")  // récupére la porchaine ligne non vide i.e l'auteur
+	    			{
+	    				getline(fichierout,S);	 
+	    			}
+	    			cout << S<<endl;
+	    			fichierin<<"Auteur: "<<S<<endl;
+	    		
+	    		}
+
+	    		// sinon on cherche l'abstract
+	    		else 
+	    		{
+
 		    		size_t abstend = s.find("Abstract");
 		    		if(abstend!=string::npos && abstract==false)
 		    		{
 		    			abstract=true;
-			    		fichierin<<"Resumer de l'article:"<<s.substr(0,abstend);
+			    		fichierin<<"Resumé de l'article:"<<s.substr(0,abstend);
 			    		while(getline(fichierout,s))
 			    		{
 			    			if(s.compare("")==0)
